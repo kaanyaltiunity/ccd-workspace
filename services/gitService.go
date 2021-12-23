2656110a20b9bf17150022dc54108ae2a3086940
kaanyaltiunity <kaan.yalti@unity3d.com>
2021-12-23 02:13:50 -0500

    testing commit message hook

---
+++ services/gitService.go
@@ -0,0 +1,18 @@
+package services
+
+import "fmt"
+
+type GitService interface {
+	SetPreCommitHook()
+}
+
+type gitService struct {
+}
+
+func NewGitService() GitService {
+	return &gitService{}
+}
+
+func (g *gitService) SetPreCommitHook() {
+	fmt.Println("G")
+}
